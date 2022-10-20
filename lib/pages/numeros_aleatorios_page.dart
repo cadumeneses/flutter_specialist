a265f6d615ce2e72ec9e97f14359e135e9978ffe
cadumeneses <carloseduardoms646@gmail.com>
2022-10-20 09:57:08 -0300

    thrid commit

---
+++ lib/pages/numeros_aleatorios_page.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class NumerosAleatoriosPage extends StatefulWidget {
+  const NumerosAleatoriosPage({super.key});
+
+  @override
+  State<NumerosAleatoriosPage> createState() => _NumerosAleatoriosPageState();
+}
+
+class _NumerosAleatoriosPageState extends State<NumerosAleatoriosPage> {
+  int? numeroGerado;
+  int? qtdCliques;
+  final String chaveAleatorio = "numero-aleatorio";
+  final String chaveQtdCliques = "quantidade_cliques";
+  late SharedPreferences storage; 
+
+  @override
+  void initState() {
+    super.initState();
+    carregaDados();
+  }
+
+  void carregaDados() async {
+    storage = await SharedPreferences.getInstance();
+    setState(() {
+      numeroGerado = storage.getInt(chaveAleatorio);
+      qtdCliques =  storage.getInt(chaveQtdCliques);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Gerador de números aleátorios'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                numeroGerado == null ? "Nenhum número gerado" : numeroGerado.toString(),
+                style: const TextStyle(fontSize: 40),
+              ),Text(
+                qtdCliques == null ? "Nenhum clique" : qtdCliques.toString(),
+                style: const TextStyle(fontSize: 40),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var random = Random();
+            setState(() {
+              numeroGerado = random.nextInt(1000);
+              qtdCliques = (qtdCliques ?? 0) + 1;
+            });
+            storage.setInt(chaveAleatorio, numeroGerado!);
+            storage.setInt(chaveQtdCliques, qtdCliques!);
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
